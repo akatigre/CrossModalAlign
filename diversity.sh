45fea993e5418f1e80f440cab754e056088788ae
JennyJeoni <yoonkim313@kaist.ac.kr>
2021-11-11 16:36:31 +0900

    Delete diversity.sh

--- diversity.sh
+++
@@ -1,2 +0,0 @@
-# (CUDA_VISIBLE_DEVICES=0 python test_diversity.py --wandb --dir "global/results/Random/entangle";) &&
-(CUDA_VISIBLE_DEVICES=0 python test_diversity.py --wandb --dir "global/results/Random/disentangle";CUDA_VISIBLE_DEVICES=1 python test_diversity.py --wandb --dir "global/results/Baseline/entangle";)
