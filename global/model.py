40258e642a53bb59fba4942300283cd989868f16
JennyJeoni <yoonkim313@kaist.ac.kr>
2022-06-01 15:27:53 +0900

    Update model.py

--- global/model.py
+++ global/model.py
@@ -18,6 +18,82 @@ from criteria.id_loss import IDLoss
 from utils.utils import l2norm
 from sklearn.neighbors import LocalOutlierFactor
 from sklearn.neighbors.kde import KernelDensity
+from scipy.signal import find_peaks
+from matplotlib import pyplot as plt
+import pandas as pd
+import seaborn as sns
+from matplotlib import gridspec
+from scipy import stats
+
+def kde_bivariate_plot(df):
+    cl = ['b','y','r', 'g', 'm', 'k'] # Custom list of colours for each categories - increase as needed...
+
+    headers = list(df.columns) # Extract list of column headers
+    # Find min and max values for all x (= col [0]) and y (= col [1]) in dataframe:
+    xmin, xmax = df.min(axis=0)[0], df.max(axis=0)[0]
+    ymin, ymax = df.min(axis=0)[1], df.max(axis=0)[1]
+    # Create a list of all unique categories which occur in the right hand column (ie index '2'):
+    category_list = df.iloc[:,2].unique()
+
+    # Set up 4 subplots and aspect ratios as axis objects using GridSpec:
+    gs = gridspec.GridSpec(2, 2, width_ratios=[1,3], height_ratios=[3,1])
+    # Add space between scatter plot and KDE plots to accommodate axis labels:
+    gs.update(hspace=0.3, wspace=0.3)
+
+    fig = plt.figure() # Set background canvas colour to White instead of grey default
+    fig.patch.set_facecolor('white')
+
+    ax = plt.subplot(gs[0,1]) # Instantiate scatter plot area and axis range
+    ax.set_xlim(xmin, xmax)
+    ax.set_ylim(ymin, ymax)
+    ax.set_xlabel(headers[0], fontsize = 14)
+    ax.set_ylabel(headers[1], fontsize = 14)
+    ax.yaxis.labelpad = 5 # adjust space between x and y axes and their labels if needed
+    ax.xaxis.labelpad = 0
+
+    axl = plt.subplot(gs[0,0], sharey=ax) # Instantiate left KDE plot area
+    axl.get_xaxis().set_visible(False) # Hide tick marks and spines
+    axl.get_yaxis().set_visible(False)
+    axl.spines["right"].set_visible(False)
+    axl.spines["top"].set_visible(False)
+    axl.spines["bottom"].set_visible(False)
+
+    axb = plt.subplot(gs[1,1], sharex=ax) # Instantiate bottom KDE plot area
+    
+    axb.get_xaxis().set_visible(False) # Hide tick marks and spines
+    axb.get_yaxis().set_visible(False)
+    axb.spines["right"].set_visible(False)
+    axb.spines["top"].set_visible(False)
+    axb.spines["left"].set_visible(False)
+
+    axc = plt.subplot(gs[1,0]) # Instantiate legend plot area
+    axc.axis('off') # Hide tick marks and spines
+
+    # For each category in the list...
+    for n in range(0, len(category_list)):
+    # Create a sub-table containing only entries matching current category:
+        st = df.loc[df[headers[2]] == category_list[n]]
+        # Select first two columns of sub-table as x and y values to be plotted:
+        x = st[headers[0]]
+        y = st[headers[1]]
+
+        # Plot data for each categorical variable as scatter and marginal KDE plots:    
+        ax.scatter(x,y, color='none', s=100, edgecolor= cl[n], label = category_list[n])
+
+        kde = stats.gaussian_kde(x, bw_method=0.1)
+        xx = np.linspace(xmin, xmax, 1000)
+        axb.plot(xx, kde(xx), color=cl[n])
+
+        kde = stats.gaussian_kde(y, bw_method=0.1)
+        yy = np.linspace(ymin, ymax, 1000)
+        axl.plot(kde(yy), yy, color=cl[n])
+
+    # Copy legend object from scatter plot to lower left subplot and display:
+    # NB 'scatterpoints = 1' customises legend box to show only 1 handle (icon) per label 
+    handles, labels = ax.get_legend_handles_labels()
+    axc.legend(handles, labels, title = headers[2], scatterpoints = 1, loc = 'center', fontsize = 12)
+    plt.savefig('plot.png', dpi=1800)
+    plt.clf()
 
 def bool2idx(b_list):
     tmp = [idx for idx, state in enumerate(b_list) if state]
@@ -36,7 +112,7 @@ class CrossModalAlign(CLIPLoss):
     def __init__(self, args):
         super().__init__(opts=args)
         self.args = args
-        self.idloss = IDLoss(args).to(args.device)
+        # self.idloss = IDLoss(args).to(args.device)
         
     def cross_modal_surgery(self, fixed_weight=False):
         """
@@ -44,7 +120,11 @@ class CrossModalAlign(CLIPLoss):
         """
         # Target Text Dissection
         text_probs = (self.text_feature @ self.prototypes.T)
-        core_mask, peri_mask = self.break_down(text_probs) # return in numpy arrays (6048, )
+        df = self.break_down(text_probs)
+        core_mask = np.array(df['categories']=='core')
+        peri_mask = np.array(df['categories']=='peripheral')
+        
+        # core_mask, peri_mask = self.break_down(text_probs) # return in numpy arrays (6048, )
 
         # VERSION 1 : DIRECTLY MANIPULATE THE CHANNELS
         # Initialize the result array 
@@ -99,33 +179,54 @@ class CrossModalAlign(CLIPLoss):
         X = X.squeeze(0)
         return l2norm((X.dot(B.T)/B.dot(B) * B).unsqueeze(0)).cuda()
 
-    def break_down(self, probs):
-        clf = LocalOutlierFactor(algorithm='auto', metric='cityblock')
+    def break_down(self, probs, plot=False):
+        clf = LocalOutlierFactor(algorithm='auto')
         probs = probs.T.cpu().detach().numpy()
-        _ = clf.fit_predict(probs)
-        lof_score = clf.negative_outlier_factor_
-        #! Kernel Density Estimation
-        kde = KernelDensity(kernel='exponential', bandwidth=0.05).fit(lof_score.reshape(-1, 1))
-        s = linspace(np.min(lof_score), np.max(lof_score))
-        e = kde.score_samples(s.reshape(-1, 1)) # reshape a single feature
-        from scipy.signal import find_peaks, peak_prominences
-        mi = find_peaks(-e)[0]
-        a, b = mi[0], mi[-1]
-        core_mask, peri_mask = np.array([lof_score < s[a]]).squeeze(0), np.array([(lof_score>=s[a])*(lof_score<s[b])]).squeeze(0)
-        return core_mask, peri_mask    
-
-
-    def evaluation(self, img_orig, img_gen, target):
-        """
-        Evaluates manipulative quality in the generated image
-        """
-        # Identity Loss(ArcFace)
-        if self.args.dataset != "AFHQ":
-            identity = self.idloss(img_orig, img_gen)[0]
+        _ = clf.fit_predict(np.abs(probs))
+        lof_score = -clf.negative_outlier_factor_
+        kde = stats.gaussian_kde(np.abs(probs).flatten(), weights=lof_score)
+        # kde = stats.gaussian_kde(np.vstack([lof_score.flatten(), probs.flatten()]),  bw_method='silverman')
+        s1 = linspace(np.min(np.abs(probs)), np.max(np.abs(probs)), 100)
+        # s2 = linspace(np.min(probs), np.max(probs), 100)
+        kernel = kde(s1)
+        # kde = KernelDensity(kernel='linear', bandwidth=0.01).fit(lof_score.reshape(-1, 1))
+        
+        # e = kde.score_samples(s.reshape(-1, 1)) # reshape a single feature
+        mi = find_peaks(kernel)[0]
+        print(mi)
+        a, b = mi[-2], mi[-1]
+        lof_score = lof_score.flatten()
+        categories = ['unwanted' if c1 else 'peripheral' if c2 else 'core' for c1, c2 in zip(np.abs(probs) < s1[a],(np.abs(probs)>=s1[a])*(np.abs(probs)<s1[b]))]
+        
+        # core_mask, peri_mask = .squeeze(0), np.array().squeeze(0)
+        df = pd.DataFrame(
+            {
+            'probs': np.abs(probs).flatten(), 
+            'lof': np.log(lof_score).flatten(),
+            'categories': categories,
+            }
+            )
+        if plot:
+            kde_bivariate_plot(df)
+            exit()
         else:
-            identity = 0
+            pass
+        return df
+        
+        
+
+
+    # def evaluation(self, img_orig, img_gen, target):
+    #     """
+    #     Evaluates manipulative quality in the generated image
+    #     """
+    #     # Identity Loss(ArcFace)
+    #     if self.args.dataset != "AFHQ":
+    #         identity = self.idloss(img_orig, img_gen)[0]
+    #     else:
+    #         identity = 0
             
-        return identity
+    #     return identity
 
     def postprocess(self, random_text_feature):
         image_manifold = l2norm(self.image_semantics.sum(dim=0, keepdim=True))
